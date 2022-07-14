efa32e5dc116b33ff3ab9538eb476b2b5cae126d
Sowwtky <miche46ale@gmail.com>
2022-07-14 16:41:47 +0200

    Bonus completato

---
+++ src/main/java/jana60/controller/MainController.java
@@ -0,0 +1,28 @@
+package jana60.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import jana60.model.University;
+import jana60.repository.UniversityRepository;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	@Autowired
+	private UniversityRepository repo;
+	
+	@GetMapping
+	public String home(Model model) {
+		List<University> uniList = (List<University>) repo.findAll();
+		model.addAttribute("uniList", uniList);
+		return "home";
+	}
+}
---
+++ src/main/java/jana60/repository/UniversityRepository.java
@@ -0,0 +1,11 @@
+package jana60.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jana60.model.University;
+
+@Repository
+public interface UniversityRepository extends CrudRepository<University, Integer>{
+
+}
